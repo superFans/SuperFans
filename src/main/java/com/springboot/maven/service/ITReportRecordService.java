e8392496e523ca82adcf8fd2b2e05c7a95e695c4
superFans <1103393971@qq.com>
2019-07-20 00:28:37 +0800

    导入日志数据 获取企业的所有模板

--- src/main/java/com/springboot/maven/service/ITReportRecordService.java
+++ src/main/java/com/springboot/maven/service/ITReportRecordService.java
@@ -21,5 +21,24 @@ public interface ITReportRecordService extends IService<TReportRecord> {
      * @return
      */
 
-    public List<Map<String, Object>> getAllUserLoggingByCompany(long size);
+    public List<Map<String, Object>> getAllUserReportByCompany(String token);
+
+    /**
+     * 导入日志数据
+     *
+     * @return
+     */
+    public Map<String, Object> importEveryDayReportData(String token);
+
+    /**
+     * @Title:获取企业的所有模板
+     * @param:
+     * userid 可选，不传表示获取企业的所有模板
+     * offset 分页游标，从0开始。根据返回结果里的next_cursor是否为空来判断是否还有下一页，且再次调用时offset设置成next_cursor的值
+     * size 分页查询的每页大小，最大100
+     * @return  List<Map<String, Object>>
+     * @author: FSS
+     */
+    public List<Map<String, Object>> reportTemplateList(String userid , String token)
+
 }
